1dce2aa6032473351026e99846d4b63ee77c6eab
Luiz Eduardo <luizeof@gmail.com>
2019-02-03 18:45:01 -0200

    Update build-shc.sh

--- build-shc.sh
+++ build-shc.sh
@@ -4,7 +4,7 @@ for filename in /shc-data/bash/*.sh; do
 
     for ((i=0; i<=3; i++)); do
 
-        shc -f $filename -o "$filename-build"
+        shc -r -f $filename -o "$filename-build"
 
     done
 
